2a88220dcf8cc1012c47d4b4cbf1b7ac1ed1062c
hypercross <hx.hyper@qq.com>
2019-02-27 12:58:01 +0800

    orphanize

---
+++ build/index.js
@@ -0,0 +1,4 @@
+export {default as egret} from "./egret/egret.web";
+export {default as dragonBones} from "./dragonBones/dragonBones";
+export {default as RES} from "./res/res";
+export {default as game} from "./game/game";
